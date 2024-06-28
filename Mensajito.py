8541bc65dc7d6edd38918da0c277e63211ef5e45
Dryn-tss <solavarria@uc.cl>
2024-06-28 15:20:17 -0400

    init

--- Mensajito.py
+++ Mensajito.py
@@ -13,12 +13,16 @@ msgOnEncode = str.encode(msgOn)
 msgOffEncode = str.encode(msgOff)
 msgOr_rightEncode = str.encode(msg_Or_right)
 msg_Or_leftEncode = str.encode(msg_Or_left)
+#hay que redefinir las variables para el robot 
 pid = PID(2.6,0,0, setpoint = 0)
 
-
-
-def enviar(vel:str):
-	msg = vel
+def enviar(vel:str, modo):
+	if modo == "orientacion":
+		msg = "O" + vel
+	if modo == "avance":
+		msg = "A" + vel
+	else: 
+		msg = "F" + 0
 	msg_e = str.encode(msg)
 	ser.write(msg_e)
 
@@ -33,14 +37,19 @@ while(True):
 
 	dis, theta = ver(ret, img)
 	print(f"distancia: {dis:3f}, angulo: {theta:3f}")
-	pid(theta)
 	pid(dis)
 
-	if dis > 30:
-		vel = 0
-		pass
+	if theta > 5:
+		vel = pid(theta)
+		enviar(str(vel), "orientacion")
+
+	if theta < 5 and dis > 30:
+		vel = pid(dis)
+		enviar(str(vel), "avance")
 	
-	enviar(str(0))
+	elif theta < 5 and dis < 30:
+		vel = 0
+		enviar(str(vel), "parar")
 	
 	# Press 'q' to exit the loop
 	if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -48,5 +57,3 @@ while(True):
 		cv2.destroyAllWindows()
 		ser.close()
 		exit()
-
-	
