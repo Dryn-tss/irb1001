463389f267e0315b9cb33ed8219782d4ee2a5b16
Dryn-tss <solavarria@uc.cl>
2024-07-04 23:50:44 -0400

    orient to ball

--- main.py
+++ main.py
@@ -33,7 +33,26 @@ def close():
     cv2.destroyAllWindows()
     close_connection()
     exit()
-		
+
+def orient_to_ball(pid, channel, theta):
+    while np.abs(theta) > 5:
+        vel = pid(theta)
+        send_msg(channel, str(vel), "orientation")
+
+
+        ret, img = vid.read()
+        if not ret:
+            break
+        dis, theta, robot_center, img_center = ver(ret, img)
+    
+    return (dis, theta, robot_center, img_center)
+
+def move_to_img_center():
+    pass
+
+
+    
+
 if __name__ == "__main__":
 
     pid, channel, vid = setup()
@@ -48,10 +67,19 @@ if __name__ == "__main__":
             dis, theta, robot_center, img_center = ver(ret, img)
             # print(f"distancia: {dis:3f}, angulo: {theta:3f}")
 
+            if cv2.waitKey(25) & 0xFF == ord('o'):  # Press 'o' to orient towards the ball 
+                dis, theta, robot_center, img_center = orient_to_ball(theta)
 
-            if np.abs(theta) > 5:
-                vel = pid(theta)
-                send_msg(channel, str(vel), "orientation")
+            if cv2.waitKey(25) & 0xFF == ord('c'):  # Press 'c' to move to img_center 
+                dis, theta, robot_center, img_center = move_to_img_center()
+            
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit 
+                close()
+
+
+
+
 
             if np.Abs(theta) < 5 and dis > 30:
                 vel = pid(dis)
@@ -60,7 +88,6 @@ if __name__ == "__main__":
             elif np.Abs(theta) < 5 and dis < 30:
                 vel = 0
                 send_msg(channel, str(vel), "stop")
-            
 
-            if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit 
-                close()
+
+            
