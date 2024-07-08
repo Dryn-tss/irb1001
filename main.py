a543285a8021cee114d2e2a145a16586c01f6812
Dryn-tss <solavarria@uc.cl>
2024-07-08 12:20:02 -0400

    fix ver

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import numpy as np
 from simple_pid import PID
 import time
 
-from perception import ver
+from perception import ver, masks
 from message import open_connection, close_connection, send_msg
 
 PATH_VID = 'vid.mp4'
+BT_PORT = "/dev/tty.IRB-G01"
 VID = False
 
 
 def setup():
     pid = PID(2.6, 0, 0, setpoint = 0)
-    channel = open_connection("/dev/tty.IRB-G01")
+    channel = open_connection(BT_PORT)
     
     if VID:
         # Open the video file 
@@ -36,23 +37,45 @@ def close():
     exit()
 
 def orient_to_ball(pid, channel, theta):
-    while abs(theta) > 5:
-        vel = pid(theta)
+    while abs(info['theta']) > 5:
+        vel = pid(info['theta'])
         send_msg(channel, str(vel), "orientation")
 
 
         ret, img = vid.read()
         if not ret:
             break
-        dis, theta, robot_center, img_center = ver(ret, img)
+        info = ver(ret, img)
     
-    return (dis, theta, robot_center, img_center)
+    return info
 
 def move_to_img_center():
     pass
 
 
-    
+
+def main():
+    img, img_masked, red_center, blue_center, yellow_center = masks(ret, img)
+    info = ver(img, img_masked, red_center, blue_center, yellow_center)
+
+
+    if abs(info['theta']) > 5:
+        vel = pid(info['theta'])
+        send_msg(channel, str(vel), "orientation")
+
+
+
+
+
+
+    if cv2.waitKey(25) & 0xFF == ord('o'):  # Press 'o' to orient towards the ball 
+        info = orient_to_ball(pid, channel, info['theta'])
+
+    if cv2.waitKey(25) & 0xFF == ord('c'):  # Press 'c' to move to img_center 
+        info = move_to_img_center()
+            
+    if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit 
+        close()
 
 if __name__ == "__main__":
 
@@ -65,43 +88,9 @@ if __name__ == "__main__":
             ret, img = vid.read()
             if not ret:
                 break
-            dis, theta, robot_center, img_center = ver(ret, img)
-            # print(f"distancia: {dis:3f}, angulo: {theta:3f}")
-
-            if abs(theta) > 5:
-                vel = pid(theta)
-                send_msg(channel, str(vel), "orientation")
-
-
-
-
-
-
-
-
-
-
-            if cv2.waitKey(25) & 0xFF == ord('o'):  # Press 'o' to orient towards the ball 
-                dis, theta, robot_center, img_center = orient_to_ball(pid, channel, theta)
-
-            if cv2.waitKey(25) & 0xFF == ord('c'):  # Press 'c' to move to img_center 
-                dis, theta, robot_center, img_center = move_to_img_center()
             
+            main()
 
-            if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit 
-                close()
-
-
-
-
-
-            # if abs(theta) < 5 and dis > 30:
-            #     vel = pid(dis)
-            #     send_msg(channel, str(vel), "advance")
-            
-            # elif abs(theta) < 5 and dis < 30:
-            #     vel = 0
-            #     send_msg(channel, str(vel), "stop")
 
 
             
